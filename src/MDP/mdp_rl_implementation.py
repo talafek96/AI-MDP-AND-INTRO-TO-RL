62398468cc1f46f3f737702eb3d88cd66fd336ae
Tal Afek <talafek@campus.technion.ac.il>
2023-01-15 04:47:51 +0200

    MDP: Implement Q-Learning

--- src/MDP/mdp_rl_implementation.py
+++ src/MDP/mdp_rl_implementation.py
@@ -1,3 +1,4 @@
+from tqdm.auto import tqdm
 from copy import deepcopy
 import random
 from typing import List, Set, Tuple
@@ -23,6 +24,22 @@ def get_actions(mdp: MDP):
 
 def get_neighbours(mdp: MDP, state: Tuple[int, int]) -> List[Tuple[int, int]]:
     return list({mdp.step(state, action) for action in get_actions(mdp)})
+
+
+def simulate_step(mdp: MDP, state: Tuple[int, int], action: str) -> Tuple[int, int]:
+    '''
+    Simulates a step in the world from `state` with the `action` given,
+    and returns the random result using the transition function of the MDP.
+    '''
+    actions = get_actions(mdp)
+    action_to_index = dict(zip(actions, range(len(actions))))
+
+    next_state_probabilities = {next_state: probability 
+                                    for next_state, probability in \
+                                        [(mdp.step(state, gt_action), mdp.transition_function[action][action_to_index[gt_action]]) \
+                                            for gt_action in actions]}
+    next_state = random.choices(list(next_state_probabilities.keys()), weights=list(next_state_probabilities.values()), k=1)[0]
+    return next_state
 
 
 def get_probability(mdp: MDP, state: Tuple[int, int], action: str, dest_state: Tuple[int, int]):
@@ -98,8 +115,8 @@ def get_policy(mdp: MDP, U: List[List[float]]):
     # ========================
 
 
-def q_learning(mdp, init_state, total_episodes=10000, max_steps=999, learning_rate=0.7, epsilon=1.0,
-                      max_epsilon=1.0, min_epsilon=0.01, decay_rate=0.8):
+def q_learning(mdp: MDP, init_state: Tuple[int, int], total_episodes=10000, max_steps=999, learning_rate=0.7, epsilon=1.0,
+                      max_epsilon=1.0, min_epsilon=0.01, decay_rate=0.8, show_progress: bool=False):
     # TODO:
     # Given the mdp and the Qlearning parameters:
     # total_episodes - number of episodes to run for the learning algorithm
@@ -114,7 +131,55 @@ def q_learning(mdp, init_state, total_episodes=10000, max_steps=999, learning_ra
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    states = get_states(mdp)
+    actions = get_actions(mdp)
+    action_to_index = dict(zip(actions, range(len(actions))))
+    index_to_action = dict(enumerate(actions))
+    state_to_index = dict(zip(states, range(len(states))))
+
+    total_episodes_gen = tqdm(range(total_episodes)) if show_progress else range(total_episodes)
+
+    qtable = np.zeros((len(states), len(actions)))
+
+    for episode in total_episodes_gen:
+        # Reset the environment
+        state = deepcopy(init_state)
+        done = False
+
+        for _ in range(max_steps):
+            # Choose an action in the current state.
+            ## First, sample a random number to determine whether to explore or exploit
+            explore_exploit_tradeoff = random.uniform(0, 1)
+
+            ## If it is greater than epsilon --> exploit (take the best/biggest Q value for this state)
+            if explore_exploit_tradeoff > epsilon:
+                chosen_action = index_to_action[np.argmax(qtable[state_to_index[state], :])]
+
+            ## Else --> explore (do a random action)
+            else:
+                chosen_action = random.sample(actions, 1)[0]
+            
+            # Take the chosen action and observe the outcome state and the corresponding reward
+            new_state = simulate_step(mdp, state, chosen_action)
+            reward = get_reward(mdp, new_state)
+            done = True if new_state in mdp.terminal_states else False
+
+            # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max (Q(s',a')) - Q(s,a)]
+            qtable[state_to_index[state], action_to_index[chosen_action]] = \
+                qtable[state_to_index[state], action_to_index[chosen_action]] + learning_rate * (reward + mdp.gamma *
+                    np.max(qtable[state_to_index[new_state], :]) - qtable[state_to_index[state], action_to_index[chosen_action]])
+            
+            # Update the current state
+            state = deepcopy(new_state)
+
+            # Finish the episode if reached a terminal state
+            if done:
+                break
+        
+        # Reduce epsilon in order to get less and less exploration
+        epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
+    
+    return qtable
     # ========================
 
 
@@ -125,7 +190,16 @@ def q_table_policy_extraction(mdp, qtable):
     #
 
     # ====== YOUR CODE: ======
-    raise NotImplementedError
+    states = get_states(mdp)
+    actions = get_actions(mdp)
+    state_to_index = dict(zip(states, range(len(states))))
+    index_to_action = dict(enumerate(actions))
+
+    def get_best_action(state: Tuple[int, int]) -> str:
+        nonlocal qtable, state_to_index, index_to_action
+        return index_to_action[np.argmax(qtable[state_to_index[state], :])]
+    
+    return [[get_best_action((i, j)) if is_state(mdp, (i, j)) else None for j in range(mdp.num_col)] for i in range(mdp.num_row)]
     # ========================
 
 
